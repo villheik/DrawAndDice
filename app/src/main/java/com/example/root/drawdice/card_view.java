902d2955e45549d09ee8873dcf8b5a606e5e42cf
Ville <wildhexes@gmail.com>
2016-12-28 03:36:36 +0200

    yö työn tulokset

---
+++ app/src/main/java/com/example/root/drawdice/card_view.java
@@ -0,0 +1,8 @@
+package com.example.root.drawdice;
+
+/**
+ * Created by keios on 28.12.2016.
+ */
+
+public class card_view {
+}
--- app/src/main/java/com/example/root/drawdice/mainView.java
+++ app/src/main/java/com/example/root/drawdice/mainView.java
@@ -1,8 +1,17 @@
 package com.example.root.drawdice;
 
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
 /**
  * Created by root on 25.12.2016.
  */
 
-public class mainView {
-}
+public class mainView extends AppCompatActivity{
+        @Override
+        protected void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.mainview);
+        }
+    }
